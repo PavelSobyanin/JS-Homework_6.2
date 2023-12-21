a6a01d055b0e136987369475d387639e43be664c
Pavel Sobyanin <pavel_sobyanin_git@mail.ru>
2023-12-21 16:43:39 +0300

    Correction commit

--- task.js
+++ task.js
@@ -1,29 +1,71 @@
-let inputBox = document.getElementById("task__input");
-let tasksList = document.getElementById("tasks__list");
+const taskInput = document.getElementById('task__input');
+let taskListArray;
+let taskListArrayRemoveButton;
 
-const removeTask = e => {
-	e.target.closest(".task").remove();
-}
 
-const addTask = e => {
-	tasksList.innerHTML +=
-		`<div class="task">
-      <div class="task__title">
-        ${inputBox.value}
-      </div>
-      <a href="#" class="task__remove">&times;</a>
-    </div>`;
+function updateLocalstorage() {
+    let storageArray = []
+    getQueries();
+    for (let item in taskListArray) {
+        storageArray.push(taskListArray[item].querySelector('div.task__title').textContent);
+    };
+    localStorage.removeItem('taskList');
+    localStorage.setItem('taskList', JSON.stringify(storageArray));
+    removeFromTaskList();
+};
 
-	inputBox.value = "";
+function loadFromStorage(){
+    if (localStorage['taskList']) {
+        let storageArray = JSON.parse(localStorage['taskList']);
+        if (localStorage.length > 0) {
+            for (let key in storageArray) {
+                if (storageArray[key]) {
+                    taskListInsert(storageArray[key]);
+                };
+            };
+        };
+    };
+    getQueries();
+};
 
-  [...(tasksList.getElementsByClassName("task__remove"))].forEach(element => {
-		element.addEventListener("click", removeTask)
-	});
+function taskListInsert(inputValue) {
+    document.getElementById('tasks__list').insertAdjacentHTML('beforeend',
+        `<div class="task">
+                 <div class="task__title">${inputValue}</div>
+                 <a href="#" class="task__remove">&times;</a>
+              </div>`
+    );
+};
 
-	e.preventDefault();
-}
+function getQueries() {
+    taskListArray = Array.from(document.querySelectorAll('div.task'));
+    taskListArrayRemoveButton = Array.from(document.querySelectorAll('a.task__remove'));
+};
 
-document.getElementById("tasks__add").addEventListener("click", addTask);
-inputBox.addEventListener("keypress", e => {
-	if (e.keyCode == 13) addTask(e);
-});
+function removeFromTaskList() {
+    if (taskListArrayRemoveButton) {
+        taskListArrayRemoveButton.forEach(function (button, index) {
+            button.onclick = function () {
+                taskListArray[index].remove();
+                updateLocalstorage();
+                return false;
+            };
+        });
+    };
+};
+
+loadFromStorage();
+
+taskInput.oninput = function () {
+    document.getElementById('tasks__add').onclick = function () {
+        if (taskInput.value) {
+            taskListInsert(taskInput.value);
+            updateLocalstorage();
+            taskInput.value = '';
+        };
+        removeFromTaskList();
+        return false;
+    };
+};
+
+removeFromTaskList();
